97ee287545d86a5e9afb0e33cb2f59a1eb7c9d99
Leehuseung <gntmd4261@naver.com>
2023-01-29 21:14:27 +0900

    boards model 정의

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import {Board} from "./board.model";
 
 //nest g service boards --no-spec
 @Injectable()
 export class BoardsService {
-    private boards = [];
+    private boards: Board[] = [];
 
-    getAllBoards() {
+    getAllBoards(): Board[] {
         return this.boards;
     }
 }
