37eb524866009ae94f1bdc6987718c22ec8cbcc7
Leehuseung <gntmd4261@naver.com>
2023-01-29 20:31:33 +0900

    create boards controller

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
-
-@Module({})
+import { BoardsController } from './boards.controller';
+//nest g co boards
+@Module({
+  controllers: [BoardsController],
+})
 export class BoardsModule {}
