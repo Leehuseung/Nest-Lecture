7a7861f64d163da78be515df719e393db529216d
Leehuseung <gntmd4261@naver.com>
2023-02-03 23:52:18 +0900

    board not found exception 처리

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -35,6 +35,7 @@ export class BoardsService {
     }
 
     deleteBoard(id: string): void {
+        const found = this.getBoardById(id);
         this.boards = this.boards.filter((board) => board.id !== id);
     }
 
