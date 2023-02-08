f556ad35d48c328fb823c5302e695361564397a4
Leehuseung <gntmd4261@naver.com>
2023-02-09 01:02:25 +0900

    UnitTest#1. Parameter Mocking

---
+++ src/payments/controllers/payments/payments.controller.ts
@@ -0,0 +1,21 @@
+import {Controller, Get, Req, Res} from '@nestjs/common';
+import {Request, Response} from 'express';
+
+@Controller('payments')
+export class PaymentsController {
+
+    @Get()
+    getPayments(@Req() request: Request, @Res() response: Response){
+        const { count, page } = request.query;
+
+        if (!count || !page) {
+            response.status(400).send({msg: 'missing count or page query parameter'});
+        }
+        else {
+            response.send(200);
+        }
+
+    }
+
+
+}
