401c73c7b26242e0e9fff907b20ecc464af01e34
Leehuseung <gntmd4261@naver.com>
2023-02-14 22:57:24 +0900

    package cat 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {typeORMConfig} from "./configs/typeorm.config";
 import {TestModule} from './test/test.module';
 import {PaymentsModule} from './payments/payments.module';
+import { CatModule } from './cat/cat.module';
 
 @Module({
     imports: [
         TypeOrmModule.forRoot(typeORMConfig),
         BoardsModule,
         TestModule,
-        PaymentsModule
+        PaymentsModule,
+        CatModule
     ],
     controllers: [],
     providers: [],
