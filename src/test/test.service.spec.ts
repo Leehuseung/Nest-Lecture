3f564c4c82cd83df0f12bcce4651a32e032c1e2e
Leehuseung <gntmd4261@naver.com>
2023-02-28 00:07:52 +0900

    describe - each

--- src/test/test.service.spec.ts
+++ src/test/test.service.spec.ts
@@ -5,16 +5,55 @@ import {TestController} from "./test.controller";
 describe('TestService', () => {
   let service: TestService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [TestController],
-      providers: [TestService],
-    }).compile();
+  // beforeEach(async () => {
+  //   const module: TestingModule = await Test.createTestingModule({
+  //     controllers: [TestController],
+  //     providers: [TestService],
+  //   }).compile();
+  //
+  //   service = module.get<TestService>(TestService);
+  // });
+  //
+  // it('should be defined', () => {
+  //   expect(service).toBeDefined();
+  // });
 
-    service = module.get<TestService>(TestService);
+
+
+});
+
+const myBeverage = {
+  delicious: true,
+  sour: false,
+};
+
+describe('my beverage', () => {
+  test('is delicious', () => {
+    expect(myBeverage.delicious).toBeTruthy();
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  test('is not sour', () => {
+    expect(myBeverage.sour).toBeFalsy();
+  });
+});
+
+describe.each([
+  [1, 1, 2],
+  [1, 2, 3],
+  [2, 1, 3],
+])('.add(%i, %i)', (a, b, expected) => {
+  test(`returns ${expected}`, () => {
+    console.log(`1 a : ${a} , b : ${b}`);
+    expect(a + b).toBe(expected);
+  });
+
+  test(`returned value not be greater than ${expected}`, () => {
+    console.log(`2 a : ${a} , b : ${b}`);
+    expect(a + b).not.toBeGreaterThan(expected);
+  });
+
+  test(`returned value not be less than ${expected}`, () => {
+    console.log(`3 a : ${a} , b : ${b}`);
+    expect(a + b).not.toBeLessThan(expected);
   });
 });
